947ce44ee3a7441cf8ef84d1731e65a0eb6edef9
Gage Norton <nortongage0@gmail.com>
2023-03-17 14:02:06 -0500

    Add post model

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+// post title, content, date created, and author
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    post_title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    })
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Comment = require('./Comment');
+const Post = require('./Post')
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
@@ -10,7 +11,12 @@ Comment.belongsTo(User, {
     onDelete: 'CASCADE',
   });
 
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
   module.exports = {
     Comment,
-    User
+    User,
+    Post
   };
