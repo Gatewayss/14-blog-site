53f18e12c9fc038690f3f3e1e19e9fd500a52b3f
Gage Norton <nortongage0@gmail.com>
2023-03-17 13:32:33 -0500

    Add homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 //const { Project, User } = require('../models');
 
+router.get('/', (req, res) => {
+    res.render('homepage');
+  });
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
    // if (req.session.logged_in) {
