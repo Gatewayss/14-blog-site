189cde72904f4924ae7ff9b923d4d94216a3b081
Gage Norton <nortongage0@gmail.com>
2023-03-20 14:09:39 -0500

    Add login routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-
-//const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api/userRoutes');
 
 router.use('/', homeRoutes);
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
