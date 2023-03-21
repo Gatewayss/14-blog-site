8b7821228bb1025ea5ca1b114d13c4d88f080476
Gage Norton <nortongage0@gmail.com>
2023-03-21 11:52:23 -0500

    Add blogs to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,8 +40,30 @@ router.get('/login', (req, res) => {
     res.render('signup');
   });
 
-  router.get('/homepage', (req, res) => {
-    res.render('homepage');
+  router.get('/homepage', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        //logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    //res.render('homepage');
   });
 
   router.get('/dashboard', (req, res) => {
