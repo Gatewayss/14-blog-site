166865363734de890f0735ce5bc9d0786d0b91b1
Gage Norton <nortongage0@gmail.com>
2024-04-19 11:42:57 -0500

    Fixed broken login route

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -25,39 +25,39 @@ const newFormHandler = async (event) => {
     }
   };
 
-  // const delButtonHandler = async (event) => {
-  //   if (event.target.hasAttribute('data-id')) {
-  //     const id = event.target.getAttribute('data-id');
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
   
-  //     const response = await fetch(`/api/post/${id}`, {
-  //       method: 'DELETE',
-  //     });
+      const response = await fetch(`/api/post/${id}`, {
+        method: 'DELETE',
+      });
   
-  //     if (response.ok) {
-  //       document.location.replace('/dashboard');
-  //     } else {
-  //       alert('Failed to delete post');
-  //     }
-  //   }
-  // };
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
 
-  // const upButtonHandler = async (event) => {
-  //   if(event.target.hasAttribute('data-update')) {
-  //     const id = event.target.getAttribute('data-id');
+  const upButtonHandler = async (event) => {
+    if(event.target.hasAttribute('data-update')) {
+      const id = event.target.getAttribute('data-id');
 
-  //     const response = await fetch(`/api/post/${id}`, {
-  //       method: 'UPDATE',
-  //     });
+      const response = await fetch(`/api/post/${id}`, {
+        method: 'UPDATE',
+      });
 
-  //     if (response.ok) {
-  //       document.location.replace('/dashboard');
-  //     } else {
-  //       alert('Failed to update post');
-  //     }
-  //   }
-  // }
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  }
 
-  // updateBtn.addEventListener('click', upButtonHandler)
-  // deleteBtn.addEventListener('click', delButtonHandler)
+  updateBtn.addEventListener('click', upButtonHandler)
+  deleteBtn.addEventListener('click', delButtonHandler)
   postForm.addEventListener('submit', newFormHandler)
  
